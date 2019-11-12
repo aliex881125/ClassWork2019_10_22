d3bf92ae7dd0f5e2c90c5b9cd7c3b2934ce98840
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-12 17:07:52 +0800

    new commit Gopher timer

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -11,26 +11,17 @@ public class GameFrame extends JInternalFrame {
      private JLabel jlb = new JLabel("分數:");
      private JLabel jlbscore = new JLabel();//分數顯示器
      private JLabel jlb1 = new JLabel("時間:");
-     private JLabel jlbtime = new JLabel("60");//時間顯示器
-     private Timer timer ;
+     private JLabel jlbtime = new JLabel();//時間顯示器
+     private JButton jbtn1 = new JButton("Add Object");
+     private Timer timer,timer1 ;
      private int score =0;
      private boolean click =true;
      private int x,y;
-
-
-     private JPanel jpn = new JPanel(new GridLayout(1,7,2,2));
-//     private JPanel jpnmos = new JPanel(new GridLayout(3,3,2,2));
-
-     private ImageIcon img = new ImageIcon("ds.png");
+     private JPanel jpn = new JPanel(new GridLayout(1,9,2,2));
+     private JPanel jpn1 = new JPanel();
+     private ImageIcon img = new ImageIcon("n5GcuQTOygzMwcjN3EzNilTOxcTMwQ3Lt92Yuc2cthWcuw2cz5SMw9yL6MHc0RHa.jpg");
+     private ImageIcon img2 = new ImageIcon("594cdbd2c4058.png");
      private JLabel jlbmos = new JLabel();
-//     private JLabel jlbmos1 = new JLabel();
-//     private JLabel jlbmos2 = new JLabel();
-//     private JLabel jlbmos3 = new JLabel();
-//     private JLabel jlbmos4 = new JLabel();
-//     private JLabel jlbmos5 = new JLabel();
-//     private JLabel jlbmos6 = new JLabel();
-//     private JLabel jlbmos7 = new JLabel();
-//     private JLabel jlbmos8 = new JLabel();
 
 
 
@@ -44,27 +35,41 @@ public class GameFrame extends JInternalFrame {
         this.setLayout(new BorderLayout(3,3));
         this.setBounds(0, 0, 800, 800);
 
+        Image img1 = img.getImage();
+        Image newing = img1.getScaledInstance(120,120, Image.SCALE_SMOOTH);
+        img =new ImageIcon(newing);
+
+        Image img3 = img2.getImage();
+        Image newing1 = img3.getScaledInstance(120,120, Image.SCALE_SMOOTH);
+        img2 =new ImageIcon(newing1);
 
 
         x=random.nextInt(GameFrame.this.getHeight()-300);
         y=random.nextInt(GameFrame.this.getWidth()-300);
         jlbmos.setBounds(x,y,100,100);
         jlbmos.setIcon(img);
-        cp.add(jlbmos);
 
 
-        timer=new Timer(800, new ActionListener() {
+
+
+        timer=new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                    x = random.nextInt(GameFrame.this.getHeight() - 200);
-                    y = random.nextInt(GameFrame.this.getWidth() - 200);
+                    x = random.nextInt(GameFrame.this.getHeight() - 300);
+                    y = random.nextInt(GameFrame.this.getWidth() - 300);
                     jlbmos.setLocation(x, y);
 
             }
         });
-
-
-
+        timer1=new Timer(500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jlbmos.setIcon(null);
+                x = random.nextInt(GameFrame.this.getHeight() - 300);
+                y = random.nextInt(GameFrame.this.getWidth() - 300);
+                jlbmos.setLocation(x, y);
+            }
+        });
 
 
 
@@ -81,64 +86,62 @@ public class GameFrame extends JInternalFrame {
         jlbtime.setBackground(new Color(255,222,100));
         jlbtime.setOpaque(true);
         jbtn.setFont(new Font(null, Font.PLAIN, 24));
-
-
-
-//        jlbmos1.setIcon(img);
-//        jlbmos2.setIcon(img);
-//        jlbmos3.setIcon(img);
-//        jlbmos4.setIcon(img);
-//        jlbmos5.setIcon(img);
-//        jlbmos6.setIcon(img);
-//        jlbmos7.setIcon(img);
-//        jlbmos8.setIcon(img);
-
-
-//        jlb.setBounds(0,0,60,40);
-//        jlbscore.setBounds(70,0,40,40);
-//        jlb1.setBounds(100,0,60,40);
-//        jlbtime.setBounds(170,0,40,40);
-//        jbtn.setBounds(170,0,60,40);
+        jbtn1.setFont(new Font(null, Font.PLAIN, 19));
 
 
         cp.add(jpn,BorderLayout.NORTH);
-//        cp.add(jpnmos,BorderLayout.CENTER);
+        cp.add(jpn1,BorderLayout.CENTER);
+
 
         jpn.add(jlb);
         jpn.add(jlbscore);
         jpn.add(jlb1);
         jpn.add(jlbtime);
         jpn.add(jbtn);
+        jpn.add(jbtn1);
+
+        jpn1.add(jlbmos);
 
 
-//
-//        jpnmos.add(jlbmos);
-//        jpnmos.add(jlbmos1);
-//        jpnmos.add(jlbmos2);
-//        jpnmos.add(jlbmos3);
-//        jpnmos.add(jlbmos4);
-//        jpnmos.add(jlbmos5);
-//        jpnmos.add(jlbmos6);
-//        jpnmos.add(jlbmos7);
-//        jpnmos.add(jlbmos8);
 
 
         jbtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
                     timer.start();
-                    cp.repaint();
+                    jlbmos.repaint();
 
             }
         });
-        cp.addMouseListener(new MouseAdapter() {
+        jlbmos.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                if(click){
-                    score++;
-                    jlbscore.setText(Integer.toString(score));
-                }
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                score++;
+                jlbscore.setText(Integer.toString(score));
+                timer.stop();
+                timer1.start();
+                jlbmos.setIcon(img2);
+                timer.start();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
 
             }
         });
